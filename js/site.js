f00f87e5969a9070096791726de828c881ac09c1
mariaantoniarosa <mariaaantonia133@gmail.com>
2023-03-08 21:31:04 -0300

    atualizações javascript

--- js/site.js
+++ js/site.js
@@ -1,31 +1,65 @@
 function validaFaleConosco() {
-	if (document.frmfaleconosco.txtnome.value == "") {
-		alert("Preencha o campo nome!");
+
+	var nome = document.frmfaleconosco.txtnome.value;
+	var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})+$");
+
+	if (!expRegNome.test(nome)) {
+		alert("Preencha o campo nome corretamente.");
 		document.frmfaleconosco.txtnome.focus();
 		return false;
-	} else if (document.frmfaleconosco.txtfone.value == "") {
-		alert("Preencha o campo telone!");
+	}
+
+	var fone = document.frmfaleconosco.txtfone.value;
+	var expRegFone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+	if (!expRegFone.test(fone)) {
+		alert("Preencha o campo telefone corretamente.")
 		document.frmfaleconosco.txtfone.focus();
 		return false;
+	}
 
-	} else if (document.frmfaleconosco.txtemail.value == "") {
+	var email = document.frmfaleconosco.txtemail.value;
+	var expRegEmail = new RegExp("^\\S+@\\S+\\.\\S+$");
+
+	if (!expRegEmail.test(email)) {
 		alert("Preencha o campo e-mail!");
 		document.frmfaleconosco.txtemail.focus();
 		return false;
+	}
+	if (!botao.checked) {
 
-	} else if (document.frmfaleconosco.selmotivo.value == 0) {
-		alert("Escolha um motivo!");
-		document.frmfaleconosco.selmotivo.focus();
-
-		if ((selmotivo == 3) && (document.frmfaleconosco.opcaoProduto == 0)) {
-			alert("Preencha o campo!")
-		}
+		alert("Por favor, selecione o botão para continuar. ");
 		return false;
+	}
 
+	if ((sexoF.checked) || (sexoM.checked)) {
+		console.log("Sexo da Pessoa Selecionado.");
+	} else {
+		alert("Por favor selecione seu Sexo!");
+		return false;
+	}
+	if ((sexoF.checked) && (sexoM.checked)) {
+		alert("Por favor selecione seu Sexo!");
+		return false;
+	}
 
+	if (document.frmfaleconosco.selmotivo.value == "") {
+		alert("Escolha um motivo!");
 
+		document.frmfaleconosco.selmotivo.focus();
+		return false;
+	}
 
-	} else if (document.frmfaleconosco.txtcomentario.value == "") {
+	var campo = document.getElementsByName("selproduto")[0];
+	if ((document.frmfaleconosco.selmotivo.value == "PR") && (campo.value == "")) {
+		alert("Escolha um produto para Informações");
+		campo.focus();
+		return false;
+	}
+
+	var texto = document.frmfaleconosco.txtcomentario.value;
+	var expRegTexto = new RegExp("^.{0}$");
+
+	if (expRegTexto.test(texto)) {
 		alert("Por favor faça um comentário!");
 		document.frmfaleconosco.txtcomentario.focus();
 		return false;
@@ -33,6 +67,7 @@ function validaFaleConosco() {
 
 	return true;
 }
+
 
 function verificaMotivo(motivo) {
 	var elemento = document.getElementById("opcaoProduto");
@@ -62,14 +97,13 @@ function verificaMotivo(motivo) {
 		select.appendChild(option);
 		elemento.appendChild(select);
 
-
 	} else {
 		if (elemento.firstChild)
 			elemento.removeChild(elemento.firstChild);
 
 	}
 
-
-
-
 }
+
+
+
